9b89aa57b8d444fb05770292a1d15d4cbecb9c5b
Michael Beaumont <mjboamail@gmail.com>
2019-07-11 22:58:33 +0200

    Add last_chance config option

--- cbatticon.c
+++ cbatticon.c
@@ -110,6 +110,7 @@ struct configuration {
     gint     icon_type;
     gint     low_level;
     gint     critical_level;
+    gboolean last_chance;
     gchar   *command_critical_level;
     gchar   *command_left_click;
 #ifdef WITH_NOTIFY
@@ -124,6 +125,7 @@ struct configuration {
     UNKNOWN_ICON,
     DEFAULT_LOW_LEVEL,
     DEFAULT_CRITICAL_LEVEL,
+    FALSE,
     NULL,
     NULL,
 #ifdef WITH_NOTIFY
@@ -164,6 +166,7 @@ static gint get_options (int argc, char **argv)
         { "icon-type"             , 'i', 0, G_OPTION_ARG_STRING, &icon_type_string                    , N_("Set icon type ('standard', 'notification' or 'symbolic')") , NULL },
         { "low-level"             , 'l', 0, G_OPTION_ARG_INT   , &configuration.low_level             , N_("Set low battery level (in percent)")                       , NULL },
         { "critical-level"        , 'r', 0, G_OPTION_ARG_INT   , &configuration.critical_level        , N_("Set critical battery level (in percent)")                  , NULL },
+        { "last-chance"           ,   0, 0, G_OPTION_ARG_NONE  , &configuration.last_chance           , N_("Check 30 seconds after critical if battery is charging")   , NULL },
         { "command-critical-level", 'c', 0, G_OPTION_ARG_STRING, &configuration.command_critical_level, N_("Command to execute when critical battery level is reached"), NULL },
         { "command-left-click"    , 'x', 0, G_OPTION_ARG_STRING, &configuration.command_left_click    , N_("Command to execute when left clicking on tray icon")       , NULL },
 #ifdef WITH_NOTIFY
